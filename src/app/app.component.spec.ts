a8e213cb35e5b749b3f8cf089d8eedb8b273d5a0
mumvock <sir_undead@hotmail.com>
2022-08-13 11:12:36 -0300

    feat: base components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,6 +16,7 @@ describe('AppComponent', () => {
         });
         fixture = TestBed.createComponent(AppComponent);
         component = fixture.componentInstance;
+        fixture.detectChanges();
     });
 
     it('should create the main component', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FooterModule } from './components/footer/footer.module';
+import { HeaderModule } from './components/header/header.module';
+import { NavModule } from './components/nav/nav.module';
+import { SidebarModule } from './components/sidebar/sidebar.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HeaderModule,
+        NavModule,
+        FooterModule,
+        SidebarModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
